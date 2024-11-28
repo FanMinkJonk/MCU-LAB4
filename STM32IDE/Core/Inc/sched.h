736986f61903214497f195ab4812a7b3b41ddc7e
FanMinkJonk <127284415+FanMinkJonk@users.noreply.github.com>
2024-11-28 23:09:16 +0700

    Edit sched.h

--- STM32IDE/Core/Inc/sched.h
+++ STM32IDE/Core/Inc/sched.h
@@ -29,24 +29,26 @@ void SCH_Update(void);
 unsigned char SCH_Add_Task(void(*pFunction)(), unsigned int DELAY, unsigned int PERIOD);
 unsigned char SCH_Delete_Task(const unsigned char TASK_INDEX);
 void SCH_Dispatch_Tasks(void);
-void SCH_Go_to_Sleep(void);
-void SCH_Report_Status();
+void SCH_Go_To_Sleep(void);
+void SCH_Report_Status(void);
 
 //--- Preprocessing ------------------------------
 //- Report Errors --------------------------------
 //#define SCH_REPORT_ERRORS
-#ifdef SCH_REPORT_ERRORS
 #define Error_port PORTA
 
-#define ERROR_SCH_TOO_MANY_TASKS 						1
-#define ERROR_SCH_WAITING_FOR_SLAVE_TO_ACK 				2
-#define ERROR_SCH_WAITING_FOR_START_COMMAND_FROM_MASTER 3
-#define ERROR_SCH_ONE_OR_MORE_SLAVES_DID_NOT_START 		4
-#define ERROR_SCH_LOST_SLAVE 							5
-#define ERROR_SCH_CAN_BUS_ERROR 						6
-#define ERROR_I2C_WRITE_BYTE_AT24C64 					7
-#define ERROR_SCH_CANNOT_DELETE_TASK					8
-#endif
+#define RETURN_NORMAL									 99
+
+#define RETURN_ERROR									100
+#define ERROR_SCH_TOO_MANY_TASKS 						101
+#define ERROR_SCH_WAITING_FOR_SLAVE_TO_ACK 				102
+#define ERROR_SCH_WAITING_FOR_START_COMMAND_FROM_MASTER 103
+#define ERROR_SCH_ONE_OR_MORE_SLAVES_DID_NOT_START 		104
+#define ERROR_SCH_LOST_SLAVE 							105
+#define ERROR_SCH_CAN_BUS_ERROR 						106
+#define ERROR_I2C_WRITE_BYTE_AT24C64 					107
+#define ERROR_SCH_CANNOT_DELETE_TASK					108
+
 
 //- Utilities ------------------------------------
 #define SCH_MAX_TASKS 	40
